755d3f3403f8d8fca4374d2b5065ff17aa1fb399
Christian Moniatis <christian.moniatis@gmail.com>
2012-03-11 20:45:28 +0100

    Global layout added

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -5,24 +5,48 @@ describe PagesController do
 
 
   describe "GET 'home'" do
-    it "returns http success" do
+    it "should return success" do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+        :content => "Ruby on Rails Tutorial Sample App | Home")
+   end
   end
+  
+  
 
   describe "GET 'contact'" do
-    it "returns http success" do
+    it "should return success" do
       get 'contact'
       response.should be_success
     end
-  end
+    
+    it 'should have the right title' do
+      get 'contact'
+      response.should have_selector("title",
+        :content => "Ruby on Rails Tutorial Sample App | Contact")
 
+    end
+  end
+  
+  
   describe "GET 'about'" do
-    it "returns http success" do
+    it "should return success" do
       get 'about'
       response.should be_success
     end
+    
+    it 'should have the right title' do
+      get 'about'
+      response.should have_selector("title",
+        :content =>  "Ruby on Rails Tutorial Sample App | About")
+
+    end
   end
+  
 
 end
